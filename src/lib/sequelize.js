5d771289283cb43fd476fe48fc90cf2562248580
fengxuan.liu <fengxuan.liu@casstime.com>
2017-09-16 15:13:25 +0800

    增加数据库测试类

--- src/lib/sequelize.js
+++ src/lib/sequelize.js
@@ -21,4 +21,15 @@ const sequelize = new Sequelize(
         }
     });
 
+sequelize.connection = () => {
+    // 测试数据库链接
+    sequelize.authenticate().then(() => {
+        console.log('数据库连接成功');
+    }).catch(err => {
+        // 数据库连接失败时打印输出
+        console.error(err);
+        throw err;
+    });
+};
+
 export default sequelize;
